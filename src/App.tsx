04043a05a9ad289a9abd11d07453686cf371de63
haj amir <amirfattahi22@gmail.com>
2024-07-29 12:01:57 +0330

    starting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter } from "react-router-dom";
+import { Navbar } from "./components";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <div className="relateive z-0 bg-primary ">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar/>
+        </div>
+        
+    </div>
+    </BrowserRouter>
+  )
 }
 
 export default App;
