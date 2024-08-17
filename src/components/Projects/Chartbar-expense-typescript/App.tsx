dcffdc77a2f13dc14da3b1e42b9a2db7e8a7099d
haj amir <amirfattahi22@gmail.com>
2024-08-17 19:25:51 +0330

    chart added

--- src/components/Projects/Chartbar-expense-typescript/App.tsx
+++ src/components/Projects/Chartbar-expense-typescript/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 import { expenses } from './expenses';
+import './index.css'
 export type Expense = {
   id: string;
   title: string;
