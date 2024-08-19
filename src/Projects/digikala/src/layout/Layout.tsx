46e3fc0c43e5544dd1a4f65f09e78a50c86bb7f8
haj amir <amirfattahi22@gmail.com>
2024-08-19 21:52:23 +0330

    digikala added

--- src/Projects/digikala/src/layout/Layout.tsx
+++ src/Projects/digikala/src/layout/Layout.tsx
@@ -1,14 +1,20 @@
+import { Helmet } from "react-helmet";
 import Digikala from "../Digikala";
 import Footer from "./Footer";
 import Header from "./Header/Header";
 
-
 function Layout() {
   return (
     <div className="bg-white digikala text-black" dir="rtl">
-      
+      <Helmet>
+        <link rel="icon" type="image/svg+xml" href="/favicon.webp"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="هر آنچه که نیاز دارید با بهترین قیمت از دیجی‌کالا بخرید! جدیدترین انواع گوشی موبایل، لپ تاپ، لباس، لوازم آرایشی و بهداشتی، کتاب، لوازم خانگی، خودرو و... با امکان تعویض و مرجوعی آسان | ✓ارسال رايگان ✓پرداخت در محل ✓ضمانت بازگشت کالا - برای خرید کلیک کنید!"/>
+
+        <title>فروشگاه اینترنتی دیجی‌کالا</title>
+      </Helmet>
       <Header />
-      <Digikala/>
+      <Digikala />
       <Footer />
     </div>
   );
