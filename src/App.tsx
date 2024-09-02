ddeb071b4a69029556226b0a93455f389b095f97
haj amir <amirfattahi22@gmail.com>
2024-09-02 21:44:16 +0330

    dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,14 @@ import ChartbarExpenseTypescript from "./Projects/Chartbar-expense-typescript/Ch
 import Layout from "./Projects/digikala/src/layout/Layout";
 import AmirFood from "./Projects/AmirFood/AmirFood";
 import AdminPanel from "./Projects/Admin-panel/AdminPanel";
+import AdminDashbord from "./Projects/Admin-dashbord/AdminDashbord";
+import OverviewPage from "./Projects/Admin-dashbord/page/OverviewPage";
+import ProductsPage from "./Projects/Admin-dashbord/page/ProductsPage";
+import UsersPage from "./Projects/Admin-dashbord/page/UsersPage";
+import SalesPage from "./Projects/Admin-dashbord/page/SalesPage";
+import OrdersPage from "./Projects/Admin-dashbord/page/OrdersPage";
+import AnalyticsPage from "./Projects/Admin-dashbord/page/AnalyticsPage";
+import SettingsPage from "./Projects/Admin-dashbord/page/SettingsPage";
 const route = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/">
@@ -27,6 +35,15 @@ const route = createBrowserRouter(
         element={<ChartbarExpenseTypescript />}
       />
       <Route path="/admin" element={<AdminPanel />} />
+      <Route path="/adminDashbord" element={<AdminDashbord />}>
+        <Route index element={<OverviewPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/users" element={<UsersPage />} />
+        <Route path="/sales" element={<SalesPage />} />
+        <Route path="/orders" element={<OrdersPage />} />
+        <Route path="/analytics" element={<AnalyticsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+      </Route>
     </Route>
   )
 );
---
+++ src/Projects/Admin-dashbord/AdminDashbord.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+
+export default function AdminDashbord() {
+  return (
+    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+      {/* BG */}
+      <div className="fixed inset-0 z-0">
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80" />
+        <div className="absolute inset-0 backdrop-blur-sm" />
+      </div>
+
+      <Sidebar />
+      <Outlet />
+    </div>
+  );
+}
